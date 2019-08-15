02db81da24028fdebd1004e23d2e4c41268a77c3
sukhbir-singh <sukhbir947@gmail.com>
2019-08-15 20:47:18 +0530

    removed authentication check for testing

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -2,8 +2,8 @@ class ApplicationController < ActionController::API
     before_action :check_authentication
 
     def check_authentication
-        if request.headers["api-key"].blank?
-           return render :json => {success: false, message: "authetication failed"}
-        end
+        # if request.headers["api-key"].blank?
+        #    return render :json => {success: false, message: "authetication failed"}
+        # end
     end
 end
